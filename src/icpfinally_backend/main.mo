b471ce943fd6201629443d84a7dce599fd739861
johnxiaohe <1154910941@qq.com>
2024-01-04 15:55:49 +0800

    init

--- src/icpfinally_backend/main.mo
+++ src/icpfinally_backend/main.mo
@@ -1,5 +1,112 @@
-actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Principal "mo:base/Principal";
+import Time "mo:base/Time";
+import Text "mo:base/Text";
+import List "mo:base/List";
+import Iter "mo:base/Iter";
+
+actor{
+
+  stable var name: Text = "";
+  stable var followed: List.List<Principal> = List.nil();
+  stable var articals: List.List<Message> = List.nil();
+
+  public type Message = {
+    content: Text;
+    ctime: Time.Time;
+    author: Principal;
   };
+
+  public type MessageVo = {
+    content: Text;
+    ctime: Time.Time;
+    author: Principal;
+    authorName: Text;
+  };
+
+  public type FollowVo = {
+    id: Principal;
+    name: Text;
+  };
+
+  public type MicroBlog = actor {
+    set_name: shared(Text) -> async();
+    get_name: shared() -> async ?Text;
+    follow: shared(Principal) -> async ();
+    follows: shared query () -> async[FollowVo];
+    post: shared(Text) -> async();
+    posts: shared query (Time.Time) -> async[MessageVo];
+    timeline: shared (Time.Time) -> async[MessageVo];
+  };
+
+  public shared func set_name(newName : Text): async (){
+    name := newName;
+  };
+
+  public shared func get_name(): async ?Text{
+    ?name;
+  };
+
+  public shared func follow(id: Principal): async (){
+    followed  := List.push(id, followed);
+  };
+
+  public shared func follows(): async[FollowVo]{
+    var result : List.List<FollowVo> = List.nil();
+    for(follow in Iter.fromList(followed)){
+      let microBlog: MicroBlog = actor(Principal.toText(follow));
+      var authorName : Text = "undifiend";
+      switch(await microBlog.get_name()){
+        case null{};
+        case (?name){
+          authorName := name;
+        };
+      };
+      let vo: FollowVo = {
+        id = follow;
+        name = authorName;
+      };
+      result := List.push(vo, result);
+    };
+    List.toArray(result);
+  };
+
+  public shared({caller}) func post(text: Text): async (){
+    let message: Message = {
+      content = text;
+      ctime = Time.now();
+      author = caller;
+    };
+    articals := List.push(message, articals);
+  };
+
+  public shared({caller}) func posts(since: Time.Time): async [MessageVo]{
+    var result: List.List<MessageVo> = List.nil();
+    for (artical in Iter.fromList(articals)){
+        if(since <= artical.ctime){
+          let vo = {
+            content = artical.content;
+            ctime = artical.ctime;
+            author = artical.author;
+            authorName = name;
+          };
+          result:= List.push(vo, result);
+        }
+    };
+    return List.toArray(result);
+  };
+
+  public shared({caller}) func timeline(since: Time.Time) :async [MessageVo]{
+    var result: List.List<MessageVo> = List.nil();
+    for(follow in Iter.fromList(followed)){
+      let microBlog: MicroBlog = actor(Principal.toText(follow));
+      let thisArticals:[MessageVo] = await microBlog.posts(since);
+      for (vo in Iter.fromArray(thisArticals)){
+        result := List.push(vo, result);
+      }
+
+    };
+    List.toArray(result);
+  }
+
+
 };
--- src/icpfinally_frontend/src/index.js
+++ src/icpfinally_frontend/src/index.js
@@ -1,19 +1,81 @@
-import { icpfinally_backend } from "../../declarations/icpfinally_backend";
+import { icpfinally_backend, createActor } from "../../declarations/icpfinally_backend";
+import moment from "moment";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+function getDate(millseconds){
+  return moment(new Date(Number(millseconds / BigInt(1000000)))).format('HH:mm:ss DD/MM/YYYY');
+}
+async function post(){
+  let post_button = document.getElementById("post");
+  post_button.disabled = true;
+  let post_input = document.getElementById("post_input");
+  let content = post_input.value.toString();
+  await icpfinally_backend.post(content);
+  post_button.disabled = false;
+  load_posts();
+}
 
-  const name = document.getElementById("name").value.toString();
+async function load_posts(){
+  let posts = await icpfinally_backend.posts(0);
+  let postOl = document.getElementById("posts");
+  postOl.innerHTML = "";
+  for (var i=0; i< posts.length; i++){
+    let n_li = document.createElement("li");
+    // n_li.setAttribute("class", "post_l");
+    let post = posts[i];
+    let l_p = document.createElement("span");
+    let p_c = document.createElement("span");
+    p_c.innerText = post.content;
+    let p_t = document.createElement("span");
+    p_t.innerText = getDate(post.ctime);
+    p_t.setAttribute("style", "float: right;");
+    l_p.appendChild(p_c);
+    l_p.appendChild(p_t);
+    n_li.appendChild(l_p);
+    postOl.appendChild(n_li);
+  }
+}
 
-  button.setAttribute("disabled", true);
+async function load_followers(){
+  let follows = await icpfinally_backend.follows();
+  let followsUl = document.getElementById("follows");
+  for (var i=0;i<follows.length; i++){
+    let follow = follows[i];
+    let n_li = document.createElement("li");
+    let n_a = document.createElement("a");
+    n_a.innerText = follow.name;
+    n_a.href = "#";
+    n_a.onclick = () => follower_posts(follow.id, follow.name);
+    n_li.appendChild(n_a);
+    followsUl.appendChild(n_li);
+  }
+}
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await icpfinally_backend.greet(name);
+async function follower_posts(canisterId, authorName){
+  let authNameEle = document.getElementById("followname");
+  authNameEle.innerText = authorName;
+  let actor = createActor(canisterId);
+  let posts = await actor.posts(0);
+  let followPostsOl = document.getElementById("followPosts")
+  followPostsOl.innerHTML = "";
+  for (var i=0; i< posts.length; i++){
+    let post = posts[i];
+    let n_li = document.createElement("li");
+    let p_c = document.createElement("span");
+    p_c.innerText = post.content;
+    let p_t = document.createElement("span");
+    p_t.innerText = getDate(post.ctime);
+    p_t.setAttribute("style", "float: right;");
+    n_li.append(p_c);
+    n_li.append(p_t);
+    followPostsOl.appendChild(n_li);
+  }
+}
 
-  button.removeAttribute("disabled");
+async function onload(){
+  let post_button = document.getElementById("post");
+  post_button.onclick = post;
+  load_posts();
+  load_followers();
+}
 
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
-});
+window.onload = onload;
